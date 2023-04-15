9142a85d6e65ebd033ef6b37e78c2ae6769a8c32
Bartosz Dudek <50589551+bdudek1@users.noreply.github.com>
2023-04-15 17:08:26 +0200

    Refactored PlayerRepository

--- src/main/java/com/example/dart/repository/PlayerRepository.java
+++ src/main/java/com/example/dart/repository/PlayerRepository.java
@@ -1,11 +1,13 @@
 package com.example.dart.repository;
 
 import com.example.dart.model.Player;
+import org.springframework.dao.DataIntegrityViolationException;
 
-import java.util.List;
+import java.util.Collection;
+import java.util.Optional;
 
 public interface PlayerRepository {
-    void savePlayer (Player player);
-    Player findPlayerById (int id);
-    List<Player> findAllPlayers();
+    void savePlayer (Player player) throws DataIntegrityViolationException;
+    Optional<Player> findPlayerById (int id);
+    Collection<Player> findAllPlayers();
 }
---
+++ src/main/java/com/example/dart/repository/impl/PlayerRepositoryHibernateImpl.java
@@ -0,0 +1,44 @@
+package com.example.dart.repository.impl;
+
+import com.example.dart.model.Player;
+import com.example.dart.repository.PlayerRepository;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Optional;
+
+@Repository
+@Transactional
+public class PlayerRepositoryHibernateImpl implements PlayerRepository {
+
+    private static final Logger logger = LoggerFactory.getLogger(PlayerRepositoryHibernateImpl.class);
+    private final SessionFactory sessionFactory;
+
+    @Autowired
+    public PlayerRepositoryHibernateImpl(SessionFactory sessionFactory) {
+        this.sessionFactory = sessionFactory;
+    }
+
+    public void savePlayer (Player player) {
+        Session session = sessionFactory.getCurrentSession();
+        session.persist(player);
+        logger.info("Player {} successfully saved.", player);
+    }
+
+    public Optional<Player> findPlayerById (int id) {
+        Session session = sessionFactory.getCurrentSession();
+        return Optional.ofNullable(session.get(Player.class, id));
+    }
+
+    public Collection<Player> findAllPlayers() {
+        Session session = sessionFactory.getCurrentSession();
+        return Collections.unmodifiableCollection(session.createQuery("from Player", Player.class).list());
+    }
+}
