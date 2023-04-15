61c90ac78034a0180019534c91a53d31c00bed32
Bartosz Dudek <50589551+bdudek1@users.noreply.github.com>
2023-04-15 17:08:34 +0200

    Refactored PlayerService

--- src/main/java/com/example/dart/service/PlayerService.java
+++ src/main/java/com/example/dart/service/PlayerService.java
@@ -5,8 +5,7 @@ import com.example.dart.model.Player;
 import java.util.Collection;
 
 public interface PlayerService {
-    void save(Player player);
-    void createPlayer(String name);
+    Player createPlayer(Player player);
     Collection<Player> findAllPlayers();
     Player findPlayerById(int id);
 }
--- src/main/java/com/example/dart/service/impl/PlayerServiceImpl.java
+++ src/main/java/com/example/dart/service/impl/PlayerServiceImpl.java
@@ -1,34 +1,47 @@
 package com.example.dart.service.impl;
 
 import com.example.dart.model.Player;
+import com.example.dart.model.exception.EntityAlreadyExistsException;
+import com.example.dart.model.exception.EntityNotFoundException;
 import com.example.dart.repository.PlayerRepository;
 import com.example.dart.service.PlayerService;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import java.util.Collection;
-import java.util.Collections;
 
 @Service
 public class PlayerServiceImpl implements PlayerService {
+    private static final Logger logger = LoggerFactory.getLogger(PlayerServiceImpl.class);
+    private final PlayerRepository playerRepository;
 
     @Autowired
-    private PlayerRepository playerRepository;
-
-    public void save(Player player) {
-        playerRepository.savePlayer(player);
+    public PlayerServiceImpl(PlayerRepository playerRepository) {
+        this.playerRepository = playerRepository;
     }
 
-    public void createPlayer(String name) {
-        Player player = new Player(name);
-        playerRepository.savePlayer(player);
+    public Player createPlayer(Player player) {
+        try {
+            playerRepository.savePlayer(player);
+        } catch (DataIntegrityViolationException e) {
+            logger.warn("Could not save player because player with name {} already exists.", player.getName());
+            throw new EntityAlreadyExistsException(e.getMessage());
+        }
+
+        return player;
     }
 
     public Collection<Player> findAllPlayers() {
-        return Collections.unmodifiableCollection(playerRepository.findAllPlayers());
+        return playerRepository.findAllPlayers();
     }
 
     public Player findPlayerById(int id) {
-        return playerRepository.findPlayerById(id);
+        return playerRepository.findPlayerById(id)
+                               .orElseThrow(EntityNotFoundException::new);
     }
 }
