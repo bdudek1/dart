ebc381804a011e97b63d7a859e01d82c88599d16
Bartosz Dudek <50589551+bdudek1@users.noreply.github.com>
2023-04-29 19:58:22 +0200

    Added PlayerStatistics to RegisteredPlayer

--- src/main/java/com/example/dart/model/RegisteredPlayer.java
+++ src/main/java/com/example/dart/model/RegisteredPlayer.java
@@ -1,13 +1,17 @@
 package com.example.dart.model;
 
 import com.example.dart.model.enums.PlayerType;
-import jakarta.persistence.DiscriminatorValue;
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
 
 @Entity
 @DiscriminatorValue("registered")
 public class RegisteredPlayer extends Player {
     private char[] password;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "player_statistics_id")
+    private PlayerStatistics playerStatistics;
+
     public RegisteredPlayer(String name, char[] password) {
         super(name);
         this.password = password;
@@ -19,5 +23,13 @@ public class RegisteredPlayer extends Player {
 
     public PlayerType getPlayerType() {
         return PlayerType.REGISTERED;
+    }
+
+    public PlayerStatistics getPlayerStatistics() {
+        return playerStatistics;
+    }
+
+    public void setPlayerStatistics(PlayerStatistics playerStatistics) {
+        this.playerStatistics = playerStatistics;
     }
 }
