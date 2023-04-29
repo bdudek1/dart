e9f030ae8010d271a7a368b6441f186c59bf3d35
Bartosz Dudek <50589551+bdudek1@users.noreply.github.com>
2023-04-29 20:00:40 +0200

    Added updating player statistics after firing a shot in GameService

--- src/main/java/com/example/dart/service/impl/GameServiceImpl.java
+++ src/main/java/com/example/dart/service/impl/GameServiceImpl.java
@@ -5,17 +5,18 @@ import com.example.dart.model.Player;
 import com.example.dart.model.Shot;
 import com.example.dart.model.dto.GameDto;
 import com.example.dart.model.enums.GameState;
+import com.example.dart.model.exception.EntityNotFoundException;
 import com.example.dart.model.exception.GameNotInProgressException;
 import com.example.dart.model.exception.NotEnoughPlayersException;
 import com.example.dart.repository.GameRepository;
 import com.example.dart.service.GameService;
+import com.example.dart.service.PlayerStatisticsService;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import com.example.dart.model.exception.EntityNotFoundException;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Collection;
 
@@ -23,10 +24,12 @@ import java.util.Collection;
 public class GameServiceImpl implements GameService {
     private static final Logger logger = LoggerFactory.getLogger(GameServiceImpl.class);
     private final GameRepository gameRepository;
+    private final PlayerStatisticsService playerStatisticsService;
 
     @Autowired
-    public GameServiceImpl(GameRepository gameRepository) {
+    public GameServiceImpl(GameRepository gameRepository,  PlayerStatisticsService playerStatisticsService) {
         this.gameRepository = gameRepository;
+        this.playerStatisticsService = playerStatisticsService;
     }
 
     public Game createGame(Collection<Player> players) {
@@ -61,6 +64,8 @@ public class GameServiceImpl implements GameService {
         game.performShot(shot);
 
         gameRepository.updateGame(game);
+
+        playerStatisticsService.updatePlayerStatistics(game, shot);
 
         logger.info("Player {} made a shot {}", game.getCurrentPlayer().getName(), shot);
 
