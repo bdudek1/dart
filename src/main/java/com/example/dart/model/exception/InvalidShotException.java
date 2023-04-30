098a9917f522e66ecb12232df76bb5c0fb1ed228
Bartosz Dudek <50589551+bdudek1@users.noreply.github.com>
2023-04-30 16:04:21 +0200

    Small refactor of InvalidShotException

--- src/main/java/com/example/dart/model/exception/InvalidShotException.java
+++ src/main/java/com/example/dart/model/exception/InvalidShotException.java
@@ -3,7 +3,7 @@ package com.example.dart.model.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(value=HttpStatus.BAD_REQUEST, reason="Invalid shot. Please select score between 0 and 20.")
+@ResponseStatus(value=HttpStatus.BAD_REQUEST, reason="Invalid shot. Please select score between 0 and 20 or 25.")
 public class InvalidShotException extends RuntimeException {
     public InvalidShotException() {
         super();
