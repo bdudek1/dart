9f4fc755437cd160480d7bf4d1db65a10da9844e
Bartosz Dudek <50589551+bdudek1@users.noreply.github.com>
2023-05-13 17:14:27 +0200

    Fixed contextLoads test

--- src/test/java/com/example/dart/DartApplicationTests.java
+++ src/test/java/com/example/dart/DartApplicationTests.java
@@ -3,10 +3,10 @@ package com.example.dart;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Profile;
-import org.springframework.context.annotation.PropertySource;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
-@PropertySource("classpath:application-test.properties")
+@TestPropertySource("classpath:application-test.properties")
 @Profile("test")
 class DartApplicationTests {
 
